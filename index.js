b1bb990e5d26ec02e106a8c6f11e541552916627
Jim Higson <jim@wikizzle.org>
2013-11-12 22:18:55 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var express = require('express'),
+    app = express();
+
+app
+   .use(express.static('statics'))
+   .use(express.static('components/oboe/dist'))
+   .use(express.static('components/jquery'))
+   .use(express.static('components/d3'))
+   .listen(8888);

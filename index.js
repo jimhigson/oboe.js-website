79f0ea4fb4b12e04edfb2e3ee35f5456516e237a
Jim Higson <jim@wikizzle.org>
2013-11-19 19:05:59 +0000

    allow root dir to work

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var express = require('express'),
     app = express();
 
 app
+   .get('/', function(req, res){
+      res.sendfile('statics/html/index.html');
+   })
    .use(express.static('statics'))
    .use(express.static('components/oboe/dist'))
    .use(express.static('components/jquery'))

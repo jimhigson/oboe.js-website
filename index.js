8373ec7f5064dfa52c237ed7c50645d76b25e9eb
Jim Higson <jim.higson@gmail.com>
2013-11-22 13:54:12 +0000

    start change to express

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 
 var express = require('express'),
-    app = express();
+    app = express(),
+    cons = require('consolidate');
+
+app.engine('html', cons.mustache);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 app
    .get('/', function(req, res){
-      res.sendfile('statics/html/index.html');
+      res.render('index', {content:'woo'});
    })
    .use(express.static('statics'))
    .use(express.static('components/oboe/dist'))
    .use(express.static('components/jquery'))
    .use(express.static('components/d3'))
+   /* app.use(function(err, req, res, next){
+        if( err ){
+            res.status(500);
+            res.render('error', { error: err });
+        }
+    }) */
    .listen(8888);

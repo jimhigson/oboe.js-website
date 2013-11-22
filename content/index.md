3d709eaa19f21b128933662467fb44d7a15723d3
Jim Higson <jim.higson@gmail.com>
2013-11-22 18:32:15 +0000

    read markdown into templates

---
+++ content/index.md
@@ -0,0 +1,12 @@
+
+Stop waiting for requests to complete by using the content
+as soon as it arrives. Select any object, array, or value from
+the http response while it is ongoing.
+
+React quickly, even downloading large content on slow networks.
+
+Connect to any standard JSON REST service or streaming REST
+service from the same client library.
+
+When a request fails halfway use the part that transferred
+without writing special cases.
--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 
 var express = require('express'),
     app = express(),
-    cons = require('consolidate');
+    cons = require('consolidate'),
+    fs = require('fs');
 
 app.engine('html', cons.mustache);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
+function readMarkdown(markdownFileName, callback, errCallback) {
+    fs.readFile('content/' + markdownFileName + '.md', function(err, markdownBuffer){
+        var markdownStr = markdownBuffer.toString();
+
+        require('marked')(markdownStr, function( err, html ){
+
+            callback(html);
+        });
+    });
+}
+
 app
    .get('/', function(req, res){
-      res.render('index', {content:'woo'});
+        readMarkdown('index', function( html ){
+            res.render('page', {content:html});
+        });
    })
    .use(express.static('statics'))
    .use(express.static('components/oboe/dist'))

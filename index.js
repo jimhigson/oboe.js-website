337a45c8c81a0c19c0a3e1eba4cea1fb49fc3995
Jim Higson <jim@wikizzle.org>
2013-11-23 13:38:59 +0000

    simple error handling

--- index.js
+++ index.js
@@ -9,14 +9,27 @@ app.engine('html', cons.mustache);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
-function readMarkdown(markdownFileName, callback, errCallback) {
-    fs.readFile('content/' + markdownFileName + '.md', function(err, markdownBuffer){
-    
-       var markdownStr = markdownBuffer.toString();
-       var html = supermarked(markdownStr, {ignoreMath:true});
+function readMarkdown(requestedMarkdown, callback, errCallback) {
+
+   function markdownPath(markdownFileName) {
+      return 'content/' + markdownFileName + '.md';
+   }
+  
+   fs.exists(markdownPath(requestedMarkdown), function(requestedMarkdownExists){
+   
+      var fileToRead = requestedMarkdownExists? 
+                              markdownPath(requestedMarkdown) 
+                           :  'content/404.md';
+   
+      fs.readFile(fileToRead, function(err, markdownBuffer){
        
-       callback( html);
-    });
+          var markdownStr = markdownBuffer.toString();
+          var html = supermarked(markdownStr, {ignoreMath:true});
+          
+          callback( html);
+       });   
+   });
+
 }
 
 function respondWithMarkdown(res, markdownFilename, opts){

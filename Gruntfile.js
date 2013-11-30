4c799236136a14b30bd058eee7c1df05eca14bb2
Jim Higson <jim@wikizzle.org>
2013-11-30 16:27:19 +0000

    allow server to be started using grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,32 @@
+'use strict';
+
 module.exports = function (grunt) {
-   
+
+    // set up config
+    grunt.initConfig({
+    
+        watch:{
+            server:{
+                files:['index.js'],
+                tasks:['develop:server'],
+                options: { nospawn: true }
+            }
+        },
+        
+        develop: {
+            server: {
+                file: 'index.js'
+            }
+        }
+        
+    });
+
+    // load all grunt tasks
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-develop');
+
+    // register a few tasks
+    grunt.registerTask('start-dev', ['develop:server', 'watch:server']);
+    //grunt.registerTask('start-real', ???);
+
 };

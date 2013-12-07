6dbbac2e5f40b81ff99776949c6e337cf24fe73b
Jim Higson <jim@wikizzle.org>
2013-12-07 14:20:55 +0100

    only show message when the location is known

--- statics/js/demo/demoView.js
+++ statics/js/demo/demoView.js
@@ -186,9 +186,10 @@ var MessageView = extend(ThingView, function(subject, demoView){
     ThingView.apply(this,arguments);
 
     this.initDomFromTemplate('messages', 'message', subject.name);
+    this.jDom.hide();
     
     subject.events('startMove').on(function(xyFrom, xyTo, duration){
-        
+        this.jDom.show();        
         goToXy(   this.jDom, 'lineX2', 'lineY2', xyFrom);
         animateXy(this.jDom, 'lineX1', 'lineY1', xyFrom, xyTo, duration);
     }.bind(this));

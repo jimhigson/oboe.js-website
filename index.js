9445962e0cd8c8d0cb3ceda277b2ff6b68d70e74
Jim Higson <jim@wikizzle.org>
2013-12-15 16:08:35 +0000

    move js source list out

--- index.js
+++ index.js
@@ -9,40 +9,7 @@ var express = require('express'),
     
     PORT = '8888',
 
-    UNMINIFIED_SCRIPTS = [
-        "/jquery/jquery.js"
-    ,   "/js/jquery.sticky.js"
-    ,   "/js/internalNav.js"
-    ,   "/js/demo/cssHooks.js"
-    ,   "/js/demo/functional.js"
-    ,   "/js/demo/lists.js"
-    ,   "/js/demo/singleEventPubSub.js"
-    ,   "/js/demo/pubSub.js"
-    ,   '/js/demo/scenarios.js'
-    ,   '/js/demo/scenarioBuilder.js'
-    ,   '/js/demo/oop.js'
-    ,   '/js/demo/model/announce.js'
-    ,   '/js/demo/model/direction.js'
-    ,   '/js/demo/model/Thing.js'
-    ,   '/js/demo/model/PacketHolder.js'
-    ,   '/js/demo/model/Demo.js'
-    ,   '/js/demo/model/Server.js'
-    ,   '/js/demo/model/AggregatingServer.js'
-    ,   '/js/demo/model/Packet.js'
-    ,   '/js/demo/model/Message.js'
-    ,   '/js/demo/model/Wire.js'
-    ,   '/js/demo/model/Client.js'
-    ,   "/js/demo/view/viewUtils.js"        
-    ,   "/js/demo/view/ThingView.js"
-    ,   "/js/demo/view/BarrierView.js"
-    ,   "/js/demo/view/ClientView.js"
-    ,   "/js/demo/view/DemoView.js"
-    ,   "/js/demo/view/MessageView.js"
-    ,   "/js/demo/view/ServerView.js"
-    ,   "/js/demo/view/WireView.js"
-    ,   "/js/demo/view/PacketView.js"
-    ,   "/js/demo/wire.js"
-    ],
+    UNMINIFIED_SCRIPTS = require('./sourceList.js'),
 
     CSS_STYLESHEETS = [
         "all.css"

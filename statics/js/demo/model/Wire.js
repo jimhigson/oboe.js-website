4216fb862015fd027230a615f8c7eba86ad9d866
Jim Higson <jim@wikizzle.org>
2013-12-23 16:40:58 +0000

    restructure

--- statics/js/demo/model/Wire.js
+++ statics/js/demo/model/Wire.js
@@ -1,53 +1,60 @@
-var Wire = extend( PacketHolder, function Wire(name, locations, options) {
+var Wire = (function(){
 
-    PacketHolder.apply(this, arguments);
-    this.latency = options.latency;
-    this.bandwidth = options.bandwidth;
-    this.medium = options.medium;
+    var Wire = extend( PacketHolder, function Wire(name, locations, options) {
     
-    if( !options.medium ) {
-        throw new Error('no medium for wire ' + name);
-    }
-    
-    this.events('reset').on(function(){
-        this.blockage = undefined;
-    }.bind(this));
-});
-
-Wire.newEvent = 'Wire';
-
-Wire.prototype.accept = function(packet){
-
-    packet.isOn(this);
-
-    this.events('deliveryStarted').emit(packet);
-    this.movePacket(packet);
-
-    if( !this.blockage )
-        this.propagateAfterLatency(packet);
-};
-Wire.prototype.with = {
-    'blockedBy':function( barrier ){
+        PacketHolder.apply(this, arguments);
+        this.latency = options.latency;
+        this.bandwidth = options.bandwidth;
+        this.medium = options.medium;
         
-        barrier.events('activated').on(function(){
-            
-            this.blockage = barrier;            
-        }.bind(this));
-
-        barrier.events('deactivated').on(function(){
-
-            this.blockage = null;
-        }.bind(this));        
-    }
-};
-Wire.prototype.propagateAfterLatency = function(packet){
-
-    this.schedule(function(){
-
-        if( !this.blockage ) {
-            this.propagate(packet);
-            this.events('delivered').emit(packet);
+        if( !options.medium ) {
+            throw new Error('no medium for wire ' + name);
         }
+        
+        this.events('reset').on(function(){
+            this.blockage = undefined;
+        }.bind(this));
+    });
+    
+    Wire.newEvent = 'Wire';
+    
+    Wire.prototype.accept = function(packet){
+    
+        packet.isOn(this);
+    
+        this.events('deliveryStarted').emit(packet);
+        this.movePacket(packet);
+    
+        if( !this.blockage )
+            this.propagateAfterLatency(packet);
+    };
 
-    }.bind(this), this.latency);
-};
+    Wire.prototype.with = {
+        'blockedBy':function( barrier ){
+            
+            barrier.events('activated').on(function(){
+                
+                this.blockage = barrier;            
+            }.bind(this));
+    
+            barrier.events('deactivated').on(function(){
+    
+                this.blockage = null;
+            }.bind(this));        
+        }
+    };
+    Wire.prototype.propagateAfterLatency = function(packet){
+    
+        this.schedule(function(){
+    
+            if( !this.blockage ) {
+                this.propagate(packet);
+                this.events('delivered').emit(packet);
+            }
+    
+        }.bind(this), this.latency);
+    };
+    
+    return Wire;    
+    
+}());

455f73af361c19507f20a0bd9672e3550c756a18
Jim Higson <jim@wikizzle.org>
2013-12-23 16:22:08 +0000

    remove defaults

--- statics/js/demo/model/demo.js
+++ statics/js/demo/model/demo.js
@@ -3,8 +3,9 @@ var Demo = extend(Thing, function Demo(name, options){
     Thing.apply(this, arguments);
 
     this.height = options.height;    
+    this.width = options.width;    
     this.script = pubSub();
-    this.colors = options.colors || 'categorical';
+    this.colors = options.colors;
     
     this.demo = this;  // we are our own demo
 });

4461a4fa1e22487b72c853ee256f45577e9e9d62
Jim Higson <jim@wikizzle.org>
2013-12-24 13:08:18 +0000

    file structure

--- statics/js/demo/model/AggregatingServer.js
+++ statics/js/demo/model/AggregatingServer.js
@@ -1,50 +1,55 @@
-var AggregatingServer = extend(Server, function AggregatingServer(name, locations, options){
-    Server.apply(this, arguments);
-  
-    this.parseStrategyName = options.parseStrategy;
-});
-AggregatingServer.newEvent = 'AggregatingServer';
+var AggregatingServer = (function(){
 
-AggregatingServer.prototype.accept = function(receivedPacket, sender){
-    
-    if( receivedPacket.direction == 'upstream' ) {
-
-        this.propagate(receivedPacket);
-
-        this.setupResponse();
-    } else {
-
-        this.parsers[sender.name].read(receivedPacket);
-    }
-};
-
-AggregatingServer.prototype.setupResponse = function(){
-    
-    this.parsers = this.createInputParsersForEachUpstreamNode(this.parseStrategyName);
-
-    this.throttledOutput = throttle( 
-        functor(500), 
-        this.events('packetReadyToDispatch').emit,
-        this
-    );
-    
-    multiplex(
-        this.parseStrategyName,
-        this.parsers,
-        this.throttledOutput.read
-    );
-
-    this.openOutboundMessages('downstream', function(packet){return packet});
-};
-
-AggregatingServer.prototype.createInputParsersForEachUpstreamNode = function(parseStrategyName){
-    var parsers = {},
-        nextLocations = this.nextLocationsInDirection('upstream'),
-        emitPacketParsed = this.events('packetParsed').emit;
-    
-    nextLocations.forEach(function(loc){
-        parsers[loc.name] = Parser(parseStrategyName, emitPacketParsed); 
+    var AggregatingServer = extend(Server, function AggregatingServer(name, locations, options){
+        Server.apply(this, arguments);
+      
+        this.parseStrategyName = options.parseStrategy;
     });
-     
-    return parsers;
-};
+    AggregatingServer.newEvent = 'AggregatingServer';
+    
+    AggregatingServer.prototype.accept = function(receivedPacket, sender){
+        
+        if( receivedPacket.direction == 'upstream' ) {
+    
+            this.propagate(receivedPacket);
+    
+            this.setupResponse();
+        } else {
+    
+            this.parsers[sender.name].read(receivedPacket);
+        }
+    };
+    
+    AggregatingServer.prototype.setupResponse = function(){
+        
+        this.parsers = this.createInputParsersForEachUpstreamNode(this.parseStrategyName);
+    
+        this.throttledOutput = throttle( 
+            functor(500), 
+            this.events('packetReadyToDispatch').emit,
+            this
+        );
+        
+        multiplex(
+            this.parseStrategyName,
+            this.parsers,
+            this.throttledOutput.read
+        );
+    
+        this.openOutboundMessages('downstream', function(packet){return packet});
+    };
+    
+    AggregatingServer.prototype.createInputParsersForEachUpstreamNode = function(parseStrategyName){
+        var parsers = {},
+            nextLocations = this.nextLocationsInDirection('upstream'),
+            emitPacketParsed = this.events('packetParsed').emit;
+        
+        nextLocations.forEach(function(loc){
+            parsers[loc.name] = Parser(parseStrategyName, emitPacketParsed); 
+        });
+         
+        return parsers;
+    };
+    
+    return AggregatingServer;
+}());

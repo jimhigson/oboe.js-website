906318315598e325226f6c9116060f27aff76ed5
Jim Higson <jim@wikizzle.org>
2013-12-24 13:00:27 +0000

    restructure Super

--- statics/js/demo/model/Cache.js
+++ statics/js/demo/model/Cache.js
@@ -1,7 +1,9 @@
 var Cache = (function(){
 
+    var Super = PacketHolder;
+    
     var Cache = extend(PacketHolder, function Relay(name, locations, options){
-        PacketHolder.apply(this, arguments);
+        Super.apply(this, arguments);
     });
     
     Cache.prototype.accept = function(packet) {

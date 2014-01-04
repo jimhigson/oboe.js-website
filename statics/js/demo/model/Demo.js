b088ba18100847ba71d64fc54cb898b62a4a0f7d
Jim Higson <jim@wikizzle.org>
2014-01-04 18:23:23 +0000

    extract magic number

--- statics/js/demo/model/Demo.js
+++ statics/js/demo/model/Demo.js
@@ -1,53 +1,58 @@
+var Demo = (function(){
 
-var Demo = extend(Thing, function Demo(name, options){
-    Thing.apply(this, arguments);
-
-    this.height = options.height;    
-    this.width = options.width;    
-    this.script = pubSub();
-    this.colors = options.colors;
-    this.paused = false;
-    this.inProgress = false;
-    
-    if( options.endSimulationEvent ) {
-       this.script( options.endSimulationEvent ).on(
-          function(){
-              this.schedule(this.reset.bind(this), 2500);
-          }.bind(this)
-       );
-    }
-    
-    this.demo = this;  // we are our own demo
-});
-
-Demo.newEvent = 'Demo';
-
-Demo.prototype.start = function(){
-    if( !this.inProgress ) {
-        this.startSimulation();
-        this.inProgress = true;
-        this.events('started').emit();
-    }
-};
-Demo.prototype.reset = function(){
-    if( this.inProgress ) {
-        this.events('reset').emit();
-        this.inProgress = false;
-        this.paused = false;
-    }
-};
-
-Demo.prototype.pause = function(){
-    this.paused = true;
-    this.events('paused').emit();
-};
-Demo.prototype.unpause = function(){
-    if( !this.paused ){
-        throw new Error('unpausing but not paused');
-    }
+   var DELAY_AFTER_FINISH = 4500;
    
-    this.paused = false;
-    this.events('unpaused').emit();
-};
+   var Demo = extend(Thing, function Demo(name, options){
+       Thing.apply(this, arguments);
+   
+       this.height = options.height;    
+       this.width = options.width;    
+       this.script = pubSub();
+       this.colors = options.colors;
+       this.paused = false;
+       this.inProgress = false;
+       
+       if( options.endSimulationEvent ) {
+          this.script( options.endSimulationEvent ).on(
+             function(){
+                 this.schedule(this.reset.bind(this), DELAY_AFTER_FINISH);
+             }.bind(this)
+          );
+       }
+       
+       this.demo = this;  // we are our own demo
+   });
+   
+   Demo.newEvent = 'Demo';
+   
+   Demo.prototype.start = function(){
+       if( !this.inProgress ) {
+           this.startSimulation();
+           this.inProgress = true;
+           this.events('started').emit();
+       }
+   };
+   Demo.prototype.reset = function(){
+       if( this.inProgress ) {
+           this.events('reset').emit();
+           this.inProgress = false;
+           this.paused = false;
+       }
+   };
+   
+   Demo.prototype.pause = function(){
+       this.paused = true;
+       this.events('paused').emit();
+   };
+   Demo.prototype.unpause = function(){
+       if( !this.paused ){
+           throw new Error('unpausing but not paused');
+       }
+      
+       this.paused = false;
+       this.events('unpaused').emit();
+   };
+   
+   return Demo;
 
-
+}());   

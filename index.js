965b5eb2471d3acd02116192ca3d7f880fcacb0e
Jim Higson <jim.higson@gmail.com>
2014-02-10 15:02:37 +0000

    increase Oboe.js version number

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var express = require('express'),
 
     CSS_STYLESHEETS = environment == 'prod'? ["all-min.css"] : ["all.css"],
         
-    LATEST_TAG = 'v1.12.2',
+    LATEST_TAG = 'v1.12.3',
     RAW_REPO_LOCATION = 'https://raw.github.com/jimhigson/oboe.js',
     REPO_LOCATION = 'https://github.com/jimhigson/oboe.js';
 
